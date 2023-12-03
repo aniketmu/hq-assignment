c7ebd41a8010c89e74766fe188172688d352e8b5
Aniket Mukherjee <aniketmukherjee5@gmail.com>
2023-12-03 15:21:33 +0530

    sixth commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
-import { useState, useEffect } from 'react'
+import "./App.css";
+import { useState, useEffect } from "react";
 import trash from "./delete.png";
 import edit from "./edit.png";
 import search from "./search.png";
 import save from "./save.png";
-
 
 const App = () => {
   const [checkedRows, setCheckedRows] = useState([]);
@@ -13,7 +12,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [editingRow, setEditingRow] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const [clearCount, setClearCount] = useState(1)
+  const [clearCount, setClearCount] = useState(1);
   const [selectChecked, setSelectChecked] = useState(false);
   const itemsPerPage = 10;
 
@@ -40,12 +39,11 @@ const App = () => {
   const searchUser = () => {
     if (searchTerm.trim() === "") {
       setLocalData(userData);
-      setClearCount((prev) => prev + 1)
-      setCurrentPage(1)
+      setClearCount((prev) => prev + 1);
+      setCurrentPage(1);
     } else {
-      
-      setClearCount((prev) => prev + 1)
-      console.log(searchTerm)
+      setClearCount((prev) => prev + 1);
+      console.log(searchTerm);
       const lowerSearchTerm = searchTerm.toLowerCase();
       const filteredUsers = userData.filter((user) =>
         Object.values(user).some(
@@ -61,8 +59,8 @@ const App = () => {
   const handleSearchInput = (e) => {
     if (e.key === "Enter") {
       if (searchTerm.trim() === "") {
-        setLocalData(userData);  // Reset to original data when search term is empty
-        setCurrentPage(1);  // Reset the current page to the first page
+        setLocalData(userData);
+        setCurrentPage(1);
         setSearchTerm("");
       } else {
         searchUser();
@@ -71,7 +69,6 @@ const App = () => {
       setSearchTerm(e.target.value);
     }
   };
-  
 
   const deleteRow = (rowId) => {
     if (checkedRows.includes(rowId)) {
@@ -79,19 +76,18 @@ const App = () => {
       const updatedMem = userData.filter((user) => user.id !== rowId);
       setUserData((prev) => updatedMem);
       setLocalData((prev) => updated);
-  
-      // Adjust current page only if the current page becomes empty
+
       if (currentItems.length === 1) {
         const newPage = Math.max(currentPage - 1, 1);
         setCurrentPage(newPage);
       }
-  
+
       setCheckedRows((prev) => [...prev.filter((row_id) => row_id !== rowId)]);
     } else {
       return;
     }
   };
-  
+
   const deleteMultipleRows = () => {
     if (checkedRows.length === 0) {
       return;
@@ -104,18 +100,16 @@ const App = () => {
       );
       setUserData(updatedMem);
       setLocalData(updated);
-  
-      // Adjust current page only if the current page becomes empty
+
       if (currentItems.length === checkedRows.length) {
         const newPage = Math.max(currentPage - 1, 1);
         setCurrentPage(newPage);
       }
-  
+
       setCheckedRows([]);
       setSelectChecked(false);
     }
   };
-  
 
   const startEditing = (rowId) => {
     if (checkedRows.includes(rowId)) {
@@ -141,7 +135,6 @@ const App = () => {
   useEffect(() => {
     setCurrentPage(1);
   }, [clearCount]);
-  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -189,154 +182,156 @@ const App = () => {
               <th>Action</th>
             </tr>
           </thead>
-          {userData.length !== 0 ? <tbody>
-            {currentItems.map((user) => (
-              <tr
-                key={user.id}
-                className={checkedRows.includes(user.id) ? "highlighted" : ""}
-              >
-                <td>
-                  <input
-                    type="checkbox"
-                    checked={checkedRows.includes(user.id)}
-                    onChange={() => {
-                      if (checkedRows.includes(user.id)) {
-                        setCheckedRows((prev) => [
-                          ...prev.filter((rowId) => rowId !== user.id),
-                        ]);
-                      } else {
-                        setCheckedRows((prev) => [...prev, user.id]);
-                      }
-                    }}
-                  />
-                </td>
-                <td>
-                  {editingRow === user.id ? (
+          {userData.length !== 0 ? (
+            <tbody>
+              {currentItems.map((user) => (
+                <tr
+                  key={user.id}
+                  className={checkedRows.includes(user.id) ? "highlighted" : ""}
+                >
+                  <td>
                     <input
-                      type="text"
-                      value={user.name}
-                      onChange={(e) => {
-                        setLocalData((prevData) =>
-                          prevData.map((u) =>
-                            u.id === user.id
-                              ? { ...u, name: e.target.value }
-                              : u
-                          )
-                        );
-                        setUserData((prevData) =>
-                        prevData.map((u) =>
-                          u.id === user.id
-                            ? { ...u, name: e.target.value }
-                            : u
-                        )
-                      );
+                      type="checkbox"
+                      checked={checkedRows.includes(user.id)}
+                      onChange={() => {
+                        if (checkedRows.includes(user.id)) {
+                          setCheckedRows((prev) => [
+                            ...prev.filter((rowId) => rowId !== user.id),
+                          ]);
+                        } else {
+                          setCheckedRows((prev) => [...prev, user.id]);
+                        }
                       }}
                     />
-                  ) : (
-                    user.name
-                  )}
-                </td>
-                <td>
-                  {editingRow === user.id ? (
-                    <input
-                      type="text"
-                      value={user.email}
-                      onChange={(e) => {
-                        setLocalData((prevData) =>
-                          prevData.map((u) =>
-                            u.id === user.id
-                              ? { ...u, email: e.target.value }
-                              : u
-                          )
-                        );
-                        setUserData((prevData) =>
-                        prevData.map((u) =>
-                          u.id === user.id
-                            ? { ...u, email: e.target.value }
-                            : u
-                        )
-                      );
-                      }}
-                    />
-                  ) : (
-                    user.email
-                  )}
-                </td>
-                <td>
-                  {editingRow === user.id ? (
-                    <select
-                      value={user.role}
-                      onChange={(e) => {
-                        setLocalData((prevData) =>
-                          prevData.map((u) =>
-                            u.id === user.id
-                              ? { ...u, role: e.target.value }
-                              : u
-                          )
-                        );
-                        setUserData((prevData) =>
-                        prevData.map((u) =>
-                          u.id === user.id
-                            ? { ...u, role: e.target.value }
-                            : u
-                        )
-                      );
-                      }}
-                    >
-                      <option value="admin">Admin</option>
-                      <option value="member">Member</option>
-                    </select>
-                  ) : (
-                    user.role
-                  )}
-                </td>
-                <td>
-                  <button
-                    type="submit"
-                    disabled={checkedRows.length > 1 || editingRow !== null}
-                    onClick={() => startEditing(user.id)}
-                    className="edit"
-                  >
-                    <img
-                      src={edit}
-                      alt="edit"
-                      border="0"
-                      style={{ height: "15px" }}
-                    />
-                  </button>
-                  <button
-                    disabled={checkedRows.length > 1}
-                    onClick={() => deleteRow(user.id)}
-                    className="delete"
-                  >
-                    <img
-                      src={trash}
-                      alt="delete"
-                      border="0"
-                      style={{ height: "15px" }}
-                    />
-                  </button>
-                  {editingRow === user.id && (
+                  </td>
+                  <td>
+                    {editingRow === user.id ? (
+                      <input
+                        type="text"
+                        value={user.name}
+                        onChange={(e) => {
+                          setLocalData((prevData) =>
+                            prevData.map((u) =>
+                              u.id === user.id
+                                ? { ...u, name: e.target.value }
+                                : u
+                            )
+                          );
+                          setUserData((prevData) =>
+                            prevData.map((u) =>
+                              u.id === user.id
+                                ? { ...u, name: e.target.value }
+                                : u
+                            )
+                          );
+                        }}
+                      />
+                    ) : (
+                      user.name
+                    )}
+                  </td>
+                  <td>
+                    {editingRow === user.id ? (
+                      <input
+                        type="text"
+                        value={user.email}
+                        onChange={(e) => {
+                          setLocalData((prevData) =>
+                            prevData.map((u) =>
+                              u.id === user.id
+                                ? { ...u, email: e.target.value }
+                                : u
+                            )
+                          );
+                          setUserData((prevData) =>
+                            prevData.map((u) =>
+                              u.id === user.id
+                                ? { ...u, email: e.target.value }
+                                : u
+                            )
+                          );
+                        }}
+                      />
+                    ) : (
+                      user.email
+                    )}
+                  </td>
+                  <td>
+                    {editingRow === user.id ? (
+                      <select
+                        value={user.role}
+                        onChange={(e) => {
+                          setLocalData((prevData) =>
+                            prevData.map((u) =>
+                              u.id === user.id
+                                ? { ...u, role: e.target.value }
+                                : u
+                            )
+                          );
+                          setUserData((prevData) =>
+                            prevData.map((u) =>
+                              u.id === user.id
+                                ? { ...u, role: e.target.value }
+                                : u
+                            )
+                          );
+                        }}
+                      >
+                        <option value="admin">Admin</option>
+                        <option value="member">Member</option>
+                      </select>
+                    ) : (
+                      user.role
+                    )}
+                  </td>
+                  <td>
                     <button
-                      onClick={() => finishEditing(user.id)}
-                      className="save"
+                      type="submit"
+                      disabled={checkedRows.length > 1 || editingRow !== null}
+                      onClick={() => startEditing(user.id)}
+                      className="edit"
                     >
                       <img
-                        src={save}
-                        alt="save"
+                        src={edit}
+                        alt="edit"
                         border="0"
                         style={{ height: "15px" }}
                       />
                     </button>
-                  )}
-                </td>
-              </tr>
-            ))}
-          </tbody> : <div className="spinner-container">
-      <div className="loading-spinner">
-      </div>
-    </div> }
-          
+                    <button
+                      disabled={checkedRows.length > 1}
+                      onClick={() => deleteRow(user.id)}
+                      className="delete"
+                    >
+                      <img
+                        src={trash}
+                        alt="delete"
+                        border="0"
+                        style={{ height: "15px" }}
+                      />
+                    </button>
+                    {editingRow === user.id && (
+                      <button
+                        onClick={() => finishEditing(user.id)}
+                        className="save"
+                      >
+                        <img
+                          src={save}
+                          alt="save"
+                          border="0"
+                          style={{ height: "15px" }}
+                        />
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          ) : (
+            <div className="spinner-container">
+              <div className="loading-spinner"></div>
+            </div>
+          )}
         </table>
         <div className="pagination-container">
           <button
@@ -366,7 +361,9 @@ const App = () => {
           <button
             onClick={() => handlePageChange(currentPage + 1)}
             disabled={
-              currentItems.length < itemsPerPage || currentItems.length === 0 ||currentPage == totalPages
+              currentItems.length < itemsPerPage ||
+              currentItems.length === 0 ||
+              currentPage === totalPages
             }
             className="next-page"
           >
@@ -374,7 +371,12 @@ const App = () => {
           </button>
           <button
             onClick={() => handlePageChange(totalPages)}
-            disabled={currentPage === totalPages || (currentPage === totalPages && (currentItems.length - itemsPerPage < 10)) || currentItems.length === 0}
+            disabled={
+              currentPage === totalPages ||
+              (currentPage === totalPages &&
+                currentItems.length - itemsPerPage < 10) ||
+              currentItems.length === 0
+            }
             className="last-page"
           >
             Last Page
@@ -383,6 +385,6 @@ const App = () => {
       </div>
     </div>
   );
-}
+};
 
 export default App;
